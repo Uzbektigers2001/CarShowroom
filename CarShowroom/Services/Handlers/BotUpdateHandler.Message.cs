1e5568437edaad59f3f47fc34a9ade94e6ef3d10
Behzod <96018826+BehzodSafarov@users.noreply.github.com>
2022-07-21 17:32:27 +0500

    Revert "buttonlar shunaqa qilip qo'shilsa bo'ladimi"

--- CarShowroom/Services/Handlers/BotUpdateHandler.Message.cs
+++ CarShowroom/Services/Handlers/BotUpdateHandler.Message.cs
@@ -11,101 +11,56 @@ namespace CarShowroom.Services
         private  async Task HandleMessageAsync(ITelegramBotClient client, Message? message, CancellationToken cancellationToken)
         {
             ArgumentNullException.ThrowIfNull(message);
-         
-            var handler = message.Text switch
+            var result= message.Text switch 
             {
-                "/start" => await client.SendTextMessageAsync(
-                         message.Chat.Id,
-                        "O'zingizga yoqqan companyani tanlang",
-                         replyMarkup: CarButtons.BrendsButtons()),
-                "Chevrolet" => await client.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "O'zingizga yoqqan Mashinani tanlang",
-                        replyMarkup: CarButtons.ChevroletButtons()),
+                "/start" =>HandleStartButtonAsync(client,message,cancellationToken),
+                _=>Task.CompletedTask
 
-                "Mercedes-Benz" => await client.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "O'zingizga yoqqan Mashinani tanlang",
-                        replyMarkup: CarButtons.MersadesButtons()),
-
-                "Hyundai" => await client.SendTextMessageAsync(
-                       message.Chat.Id,
-                       "O'zingizga yoqqan Mashinani tanlang",
-                       replyMarkup: CarButtons.HundayButtons()),
-
-                "BMW" => await client.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "O'zingizga yoqqan Mashinani tanlang",
-                        replyMarkup: CarButtons.BMWButtons()),
-
-                "Kia" => await client.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "O'zingizga yoqqan Mashinani tanlang",
-                        replyMarkup: CarButtons.KiyaButtons()),
-
-                "Lada" => await client.SendTextMessageAsync(
-                        message.Chat.Id,
-                        "O'zingizga yoqqan Mashinani tanlang",
-                        replyMarkup: CarButtons.LadaButtons()),
-                "Tracker" => await client.SendPhotoAsync(
-                        message.Chat.Id,
-                       photo: @"https://www.supercars.net/blog/wp-content/uploads/2020/12/2021-BMW-M3-Competition-011-2160-scaled-1.jpg",
-                        cancellationToken: cancellationToken
-                )
+                
 
             };
-
+            await result;
+            var from = message.From;
+            _logger.LogInformation("Received message from {from!.FirstName} : {message.Text}", from!.FirstName, message.Text);
+           
+           
         }
-          //  var result= message.Text switch 
-          //  {
-          //      "/start" =>HandleStartButtonAsync(client,message,cancellationToken),
-          //      _=>Task.CompletedTask
-//
-          //      
-//
-          //  };
-          //  await result;
-          //  var from = message.From;
-          //  _logger.LogInformation("Received message from {from!.FirstName} : {message.Text}", from!.FirstName, message.Text);
-           
-           
-        
 
-       // private  Task HandleUnkownMessage(ITelegramBotClient client, Message? message, CancellationToken cancellationToken)
-       // {
-       //     
-       ////     return Task.CompletedTask;
-       //// }
-       //// private  async Task HandleStartButtonAsync(ITelegramBotClient client, Message? message, CancellationToken cancellationToken)
-       //// {
-       ////     if( await _userService.Exits(message.From.Id)){
-       ////        await client.SendTextMessageAsync(
-       ////             chatId:message.Chat.Id,
-       ////             text:"You already registerd",
-       ////             replyToMessageId:message.MessageId,
-       ////             cancellationToken:cancellationToken
-       ////         );
-       ////         return;
-       ////        
-       ////      }
-////
-       ////    await _userService.AddNewUser(new UserModel(){
-       ////     Id=message.From.Id,
-  //     //     ChatId=message.Chat.Id,
-       ////     UserName=message.From.Username,
-       ////     FirstName=message.From.FirstName,
-       ////     LastName=message.From.LastName,
-       ////     LanguageCode=message.From.LanguageCode,
-       ////     CardNumber=null,
-       ////     CardBalance=null});
-       ////     await client.SendTextMessageAsync(
-       ////             chatId:message.Chat.Id,
-       ////             text:"Success added",
-       ////             replyToMessageId:message.MessageId,
-       ////             cancellationToken:cancellationToken
-       ////         );
-////
-         //  
-        //}
+        private  Task HandleUnkownMessage(ITelegramBotClient client, Message? message, CancellationToken cancellationToken)
+        {
+            
+            return Task.CompletedTask;
+        }
+        private  async Task HandleStartButtonAsync(ITelegramBotClient client, Message? message, CancellationToken cancellationToken)
+        {
+            if( await _userService.Exits(message.From.Id)){
+               await client.SendTextMessageAsync(
+                    chatId:message.Chat.Id,
+                    text:"You already registerd",
+                    replyToMessageId:message.MessageId,
+                    cancellationToken:cancellationToken
+                );
+                return;
+               
+             }
+
+           await _userService.AddNewUser(new UserModel(){
+            Id=message.From.Id,
+            ChatId=message.Chat.Id,
+            UserName=message.From.Username,
+            FirstName=message.From.FirstName,
+            LastName=message.From.LastName,
+            LanguageCode=message.From.LanguageCode,
+            CardNumber=null,
+            CardBalance=null});
+            await client.SendTextMessageAsync(
+                    chatId:message.Chat.Id,
+                    text:"Success added",
+                    replyToMessageId:message.MessageId,
+                    cancellationToken:cancellationToken
+                );
+
+           
+        }
     }
 }
